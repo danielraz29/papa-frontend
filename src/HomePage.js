f8ca09b2ea9075e771f3fbca021894ef78c0e358
Daniel Raz <danielraz@Daniels-MacBook-Air.local>
2025-05-09 13:16:28 +0300

    עדכון כתובת API ל-Render

--- src/HomePage.js
+++ src/HomePage.js
@@ -15,7 +15,7 @@ function HomePage() {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:8000/login', {
+      const response = await fetch('https://papa-backend.onrender.com/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'

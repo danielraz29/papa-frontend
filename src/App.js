262ff89b07ad45ab705d186aca90af0d54ea2919
danielraz29 <104888372+danielraz29@users.noreply.github.com>
2025-06-04 15:17:36 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import MenteeDashboard from "./MenteeDashboard"; // במידה ורלוונטי
-import MentorRequest from './MentorRequest'; // הקובץ החדש שלך
+import MentorRequest from './MentorRequest'; 
+import AdminPage from './AdminPage'; 
+import MentorHomePage. from './MentorHomePage.'; 
+import MentorSwipe from './MentorSwipe'; 
+import TraineesPage from './TraineesPage'; 
 
 
 function App() {
@@ -11,7 +15,12 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/dashboard/mentee" element={<MenteeDashboard />} />
+         <Route path="/dashboard/admin" element={<AdminPage />} />
+        <Route path="/trainees" element={<TraineesPage />} />
+         <Route path="/dashboard/mentor" element={<MentorDashboard />} />
+
         <Route path="/mentor-request" element={<MentorRequest />} />
+        <Route path="/mentor-swipe" element={<MentorSwipe />} />
       </Routes>
     </Router>
   );
